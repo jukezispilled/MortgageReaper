ee450a850ab443c6e0a8e56bde7e1709fdbae173
jukezispilled <whotfisjax@gmail.com>
2023-09-25 07:59:56 -0400

    tt

--- src/Contact.js
+++ src/Contact.js
@@ -26,10 +26,6 @@ const Contact = () => {
   });
 
   return (
-    <div className="h-screen bg-cover bg-[#ffffff] bg-fixed pb-[10%]">
-        <div className="px-[10%] mx-[8.5%] pt-[22%] md:pt-[10%] xl:pt-[8%]">
-            <div className="xl:bg-[#eef0f7] h-min xl:shadow-md rounded-xl xl:p-10 pb-[10%]">
-            <div className="text-5xl font-bold mb-7">Contact</div>
                 <div>
                     <Formik
                         initialValues={{
@@ -99,9 +95,6 @@ const Contact = () => {
                             )}
                         </Formik>
                     </div>
-                </div>
-            </div>
-        </div>
     )
 }
 
