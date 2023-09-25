1b7bf9c7f2b00694e52bb30ea82188063d837cda
jukezispilled <whotfisjax@gmail.com>
2023-09-25 07:31:34 -0400

    Allahu Akbar

--- api/email.js
+++ api/email.js
@@ -1,4 +1,36 @@
-module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello from the serverless function!' });
+const nodemailer = require('nodemailer');
+
+module.exports = async (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  // Create a transporter object using SMTP transport
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // Use the email service you prefer
+    auth: {
+      user: 'jackw1618@gmail.com', // Your email address
+      pass: 'rhchqsetnzgfntvz', // Your email password or an app password
+    },
+  });
+
+  // Email data
+  const mailOptions = {
+    from: 'jackw1618@gmail.com',
+    to: 'jackw1618@gmail.com', // The recipient's email address
+    subject: subject,
+    text: `
+      Full Name: ${name}
+      Email: ${email}
+      Subject: ${subject}
+      Message: ${message}
+    `,
   };
-  
+
+  try {
+    // Send email
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error('Email error:', error);
+    res.status(500).json({ message: 'Failed to send email' });
+  }
+};
