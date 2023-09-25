31edf7770f7d708c136885a4b415269a66170726
jukezispilled <whotfisjax@gmail.com>
2023-09-25 07:20:32 -0400

    T

--- api/email.js
+++ api/email.js
@@ -1,33 +1,4 @@
-import { createTransport } from 'nodemailer';
-///const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS, EMAIL_TO } = process.env;
-
-export default async (req, res) => {
-
-  // Create a transporter object using SMTP transport
-  const transporter = createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'jackw1618@gmail.com',
-      pass: 'uqdjdatfyjqzkaoo',
-    },
-  });
-
-  // Email data
-  const mailOptions = {
-    from: email,
-    to: 'jackw1618@gmail.com',
-    subject: 'New Inquiry from Your Website',
-    text: `
-      Full Name:
-    `,
+module.exports = (req, res) => {
+    res.status(200).json({ message: 'Hello from the serverless function!' });
   };
-
-  try {
-    // Send email
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: 'Email sent successfully' });
-  } catch (error) {
-    console.error('Email error:', error);
-    res.status(505).json({ message: 'Failed to send email' });
-  }
-};
+  
