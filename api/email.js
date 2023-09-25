2b697f872fc400384e73223d6bd4a6c044fa3ec3
jukezispilled <whotfisjax@gmail.com>
2023-09-25 07:18:14 -0400

    Testing

--- api/email.js
+++ api/email.js
@@ -28,6 +28,6 @@ export default async (req, res) => {
     res.status(200).json({ message: 'Email sent successfully' });
   } catch (error) {
     console.error('Email error:', error);
-    res.status(500).json({ message: 'Failed to send email' });
+    res.status(505).json({ message: 'Failed to send email' });
   }
 };
